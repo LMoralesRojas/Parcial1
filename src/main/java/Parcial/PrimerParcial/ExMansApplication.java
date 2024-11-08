12a33e062fec35933a8b83d5fefe8f28c7071c5a
Lucas <lucasmorales110@gmail.com>
2024-11-08 00:48:38 -0300

    Reorganizacion final proyecto

---
+++ src/main/java/Parcial/PrimerParcial/ExMansApplication.java
@@ -0,0 +1,16 @@
+package Parcial.PrimerParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExMansApplication.class, args);
+		System.out.println("La aplicación esta corriendo");
+
+
+	}
+
+}
